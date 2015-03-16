d4f22024ef233f9e206d002bfaf3b3f6c68cc278
Menaka <menaka.k@gmail.com>
2015-03-16 18:29:30 -0400

    Fixed axis labels and title

--- plot3.R
+++ plot3.R
@@ -9,10 +9,10 @@ BmorebyYearType <- NEI[fips == "24510"][, sum(Emissions), by = list(year, type)]
 setnames(BmorebyYearType, old = c("year", "V1"), new = c("Year", "Emissions"))
 
 png("plot3.png", height = 480, width = 480)
-plot <- qplot(Year, Emissions, data = BmorebyYearType, color = type, geom = c("line", "point"))
-plot <- plot + facet_wrap(~type, ncol = 2)
-
+plot <- qplot(Year, Emissions, data = BmorebyYearType, color = type, geom = c("line", "point"), main = "Total PM25 Emissions in Baltimore from 1999-2008 by source type")
+plot <- plot + facet_wrap(~type, ncol = 2) + scale_x_continuous(breaks = c(1999, 2002, 2005, 2008))
+plot
 dev.off()
 
-
-#As the plots show,
+#As the plots show, the "Point" type of emission actually increased in Baltimore
+#from 1999-2008, while the other types decreased
