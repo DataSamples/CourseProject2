18c49860b1cf2af7afb75c067ef8f7624d547ae8
Menaka <menaka.k@gmail.com>
2015-03-16 15:51:10 -0400

    Question 2

---
+++ plot2.R
@@ -0,0 +1,13 @@
+#This code assumes that the files have been downloaded and unzipped into
+#the working directory.
+
+#NEI <- readRDS("summarySCC_PM25.rds")
+#SCC <- readRDS("Source_Classification_Code.rds")
+#NEI <- data.table(NEI)
+
+BmorebyYear <- NEI[fips == "24510"][, sum(Emissions), by = year]
+
+png("plot2.png", height = 480, width = 480)
+plot(BmorebyYear, type = "b", xlab = "Year", ylab = "Total Emissions", main = "Total Emissions in Baltimore for the years 1999, 2002, 2005, 2008", xaxt = "n")
+axis(side = 1, at = sumsbyYear$year, labels = TRUE)
+dev.off()
